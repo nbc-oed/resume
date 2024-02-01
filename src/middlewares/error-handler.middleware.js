4800ac424133741898ca521975dda00103b2219e
ooheunda <ooheunda@gmail.com>
2024-02-02 01:07:54 +0900

    :mute: remove comment

--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,10 +1,6 @@
 export default (err, req, res, next) => {
   console.log(err.name, ":", err.message);
 
-  // if (err.name === "CastError") {
-  //   return res.status(404).json({ message: "상품이 존재하지 않습니다." });
-  // } // 모든 CastError 처리를 이 message로 할 수 없음 (일단보류)
-
   if (err.name === "ValidationError" || "PrismaClientValidationError") {
     return res.status(403).json({ message: "올바른 값을 입력해주세요." });
   }
