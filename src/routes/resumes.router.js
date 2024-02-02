928a66a94bf7216b29007ab2bf5234e91054a445
ooheunda <ooheunda@gmail.com>
2024-02-02 12:58:49 +0900

    :necktie: add resume delete API

--- src/routes/resumes.router.js
+++ src/routes/resumes.router.js
@@ -29,6 +29,9 @@ router.get("/resumes", async (req, res, next) => {
     },
   });
 
+  if (!resumes[0])
+    return res.status(404).json({ message: "이력서 데이터가 없습니다." });
+
   return res.status(200).json({ data: resumes });
 });
 
@@ -78,6 +81,8 @@ router.get("/resumes/:resumeId", async (req, res, next) => {
     },
   });
 
+  if (!resume)
+    return res.status(404).json({ message: "이력서 데이터가 없습니다." });
   return res.status(200).json({ data: resume });
 });
 
@@ -116,10 +121,29 @@ router.put("/resumes/:resumeId", authMiddleware, async (req, res, next) => {
 });
 
 // 이력서 삭제 API
-router.delete(
-  "/resumes/:resumeId",
-  authMiddleware,
-  async (req, res, next) => {}
-);
+router.delete("/resumes/:resumeId", authMiddleware, async (req, res, next) => {
+  try {
+    const { userId } = req.user;
+    const { resumeId } = req.params;
+
+    const resume = await prisma.resumes.findFirst({
+      where: { resumeId: +resumeId },
+    });
+    if (!resume)
+      return res.status(400).json({ message: "이력서가 존재하지 않습니다." });
+    if (+userId !== resume.userId)
+      return res
+        .status(401)
+        .json({ message: "본인의 이력서만 삭제할 수 있습니다." });
+
+    await prisma.resumes.delete({
+      where: { resumeId: +resumeId },
+    });
+
+    return res.status(201).json({ message: "삭제가 완료되었습니다." });
+  } catch (err) {
+    next(err);
+  }
+});
 
 export default router;
