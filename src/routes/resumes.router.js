87eb40b8e465978c30c77579183f99535fda5edd
ooheunda <ooheunda@gmail.com>
2024-02-05 02:09:26 +0900

    :zap: add order option to get resumes API

--- src/routes/resumes.router.js
+++ src/routes/resumes.router.js
@@ -6,33 +6,40 @@ const router = express.Router();
 
 // 이력서 목록 조회 API
 router.get("/resumes", async (req, res, next) => {
-  const resumes = await prisma.resumes.findMany({
-    select: {
-      resumeId: true,
-      title: true,
-      content: true,
-      status: true,
-      createdAt: true,
-      updatedAt: true,
-      user: {
-        select: {
-          userInfo: {
-            select: {
-              name: true,
+  try {
+    const { orderKey, orderValue } = req.query;
+    const key = orderKey ? orderKey : "createdAt";
+    const value = orderValue ? orderValue : "desc";
+
+    const resumes = await prisma.resumes.findMany({
+      select: {
+        resumeId: true,
+        title: true,
+        status: true,
+        createdAt: true,
+        updatedAt: true,
+        user: {
+          select: {
+            userInfo: {
+              select: {
+                name: true,
+              },
             },
           },
         },
       },
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+      orderBy: {
+        [key]: value,
+      },
+    });
 
-  if (!resumes[0])
-    return res.status(404).json({ message: "이력서 데이터가 없습니다." });
+    if (!resumes[0])
+      return res.status(404).json({ message: "이력서 데이터가 없습니다." });
 
-  return res.status(200).json({ data: resumes });
+    return res.status(200).json({ data: resumes });
+  } catch (err) {
+    next(err);
+  }
 });
 
 // 이력서 생성 API
@@ -91,7 +98,7 @@ router.put("/resumes/:resumeId", authMiddleware, async (req, res, next) => {
   try {
     const { userId } = req.user;
     const { resumeId } = req.params;
-    let { title, content, status } = req.body;
+    const { title, content, status } = req.body;
     if (!title && !content && !status)
       return res.status(400).json({ message: "수정할 값을 입력해주세요." });
 
@@ -105,13 +112,17 @@ router.put("/resumes/:resumeId", authMiddleware, async (req, res, next) => {
         .status(401)
         .json({ message: "본인의 이력서만 수정할 수 있습니다." });
 
-    title = title ? title : resume.title;
-    content = content ? content : resume.content;
-    status = status ? status : resume.status;
+    const updatedTitle = title ? title : resume.title;
+    const updatedContent = content ? content : resume.content;
+    const updatedStatus = status ? status.toUpperCase() : resume.status;
 
     await prisma.resumes.update({
       where: { resumeId: +resumeId },
-      data: { title, content, status },
+      data: {
+        title: updatedTitle,
+        content: updatedContent,
+        status: updatedStatus,
+      },
     });
 
     return res.status(201).json({ message: "수정이 완료되었습니다." });
